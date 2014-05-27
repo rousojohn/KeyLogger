a1df4226097da0dbeecba58774cdbf39d8c6e475
rousojohn <rousojohn@gmail.com>
2014-05-27 20:37:19 +0300

    CreditCardUtility class
    Added CreditCardUtility class with credit card validation functions

---
+++ Windows Local host Process/Windows Local host Process/CreditCardUtility.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Windows_Local_host_Process
+{
+    public enum CreditCardTypeType
+    {
+        Visa,
+        MasterCard,
+        Discover,
+        Amex,
+        Switch,
+        Solo,
+        DinersClub
+    }
+
+
+    /// <summary>
+    /// Look @ http://stackoverflow.com/questions/9315647/regex-credit-card-number-tests
+    /// Known Bug: Visa Number -> 4222222222222 NOT matched
+    /// </summary>
+    
+    public class CreditCardUtility
+    {
+        private const string cardRegex = "^(?:(?<Visa>4\\d{3})|(?<MasterCard>5[1-5]\\d{2})|(?<Discover>6011)|(?<DinersClub>(?:3[68]\\d{2})|(?:30[0-5]\\d))|(?<Amex>3[47]\\d{2}))([ -]?)(?(DinersClub)(?:\\d{6}\\1\\d{4})|(?(Amex)(?:\\d{6}\\1\\d{5})|(?:\\d{4}\\1\\d{4}\\1\\d{4})))$";
+
+        public static bool IsValidNumber(string cardNum)
+        {
+            Regex cardTest = new Regex(cardRegex);
+
+            //Determine the card type based on the number
+            CreditCardTypeType? cardType = GetCardTypeFromNumber(cardNum);
+
+            //Call the base version of IsValidNumber and pass the 
+            //number and card type
+            if (IsValidNumber(cardNum, cardType))
+                return true;
+            else
+                return false;
+        }
+
+        public static bool IsValidNumber(string cardNum, CreditCardTypeType? cardType)
+        {
+            //Create new instance of Regex comparer with our 
+            //credit card regex pattern
+            Regex cardTest = new Regex(cardRegex);
+
+            //Make sure the supplied number matches the supplied
+            //card type
+            if (cardTest.Match(cardNum).Groups[cardType.ToString()].Success)
+            {
+                //If the card type matches the number, then run it
+                //through Luhn's test to make sure the number appears correct
+                if (PassesLuhnTest(cardNum))
+                    return true;
+                else
+                    //The card fails Luhn's test
+                    return false;
+            }
+            else
+                //The card number does not match the card type
+                return false;
+        }
+
+
+        public static CreditCardTypeType? GetCardTypeFromNumber(string cardNum)
+        {
+            //Create new instance of Regex comparer with our
+            //credit card regex pattern
+            Regex cardTest = new Regex(cardRegex);
+
+            //Compare the supplied card number with the regex
+            //pattern and get reference regex named groups
+            GroupCollection gc = cardTest.Match(cardNum).Groups;
+
+            //Compare each card type to the named groups to 
+            //determine which card type the number matches
+            if (gc[CreditCardTypeType.Amex.ToString()].Success)
+            {
+                return CreditCardTypeType.Amex;
+            }
+            else if (gc[CreditCardTypeType.MasterCard.ToString()].Success)
+            {
+                return CreditCardTypeType.MasterCard;
+            }
+            else if (gc[CreditCardTypeType.Visa.ToString()].Success)
+            {
+                return CreditCardTypeType.Visa;
+            }
+            else if (gc[CreditCardTypeType.Discover.ToString()].Success)
+            {
+                return CreditCardTypeType.Discover;
+            }
+            else if (gc[CreditCardTypeType.DinersClub.ToString()].Success)
+            {
+                return CreditCardTypeType.DinersClub;
+            }
+            else
+            {
+                //Card type is not supported by our system, return null
+                //(You can modify this code to support more (or less)
+                // card types as it pertains to your application)
+                return null;
+            }
+        }
+
+        public static bool PassesLuhnTest(string cardNumber)
+        {
+            //Clean the card number- remove dashes and spaces
+            cardNumber = cardNumber.Replace("-", "").Replace(" ", "");
+
+            //Convert card number into digits array
+            int[] digits = new int[cardNumber.Length];
+            for (int len = 0; len < cardNumber.Length; len++)
+            {
+                digits[len] = Int32.Parse(cardNumber.Substring(len, 1));
+            }
+
+            //Luhn Algorithm
+            //Adapted from code availabe on Wikipedia at
+            //http://en.wikipedia.org/wiki/Luhn_algorithm
+            int sum = 0;
+            bool alt = false;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                int curDigit = digits[i];
+                if (alt)
+                {
+                    curDigit *= 2;
+                    if (curDigit > 9)
+                    {
+                        curDigit -= 9;
+                    }
+                }
+                sum += curDigit;
+                alt = !alt;
+            }
+
+            //If Mod 10 equals 0, the number is good and this will return true
+            return sum % 10 == 0;
+        }
+    }
+}
